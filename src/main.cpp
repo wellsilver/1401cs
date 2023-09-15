0224a601a7b24e8ec9e680dacdb8cab3c9fc760f
wellsilver <wellsilver2016@gmail.com>
2023-09-15 19:22:07 -0400

    back to sdl

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,13 @@
 
 #include "render.hpp"
 
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+
 using namespace std;
 using std::thread;
 
 int main() {
-
   auto th = thread(render);
 
   computerrun();
--- src/render.hpp
+++ src/render.hpp
@@ -1,5 +1,16 @@
-#include "lib/renderer.h"
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+
+SDL_Window *window;
+SDL_Renderer *renderer;
+SDL_Event e;
 
 void render() {
-  
+  SDL_Init(SDL_INIT_EVERYTHING);
+
+  SDL_CreateWindowAndRenderer(500,500,SDL_WINDOW_RESIZABLE,&window,&renderer);
+
+  while (1) {
+    SDL_WaitEvent(&e);
+  }
 }
