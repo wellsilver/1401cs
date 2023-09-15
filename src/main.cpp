4d40c3f65f5c2e124b726d02871c38f0d806acce
wellsilver <wellsilver2016@gmail.com>
2023-09-14 20:09:23 -0400

    sdl

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+
 using namespace std;
 
 #include "1401.hpp"
 
 int main() {
-  
+  SDL_Init(SDL_INIT_EVERYTHING);
+
   return 0;
 }
